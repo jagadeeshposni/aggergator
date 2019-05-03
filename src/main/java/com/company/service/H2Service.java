edf36bd6c57f077aed7d223495ed43720347b217
pjagade <pjagade@L-HYD01HJ9K8-M.local>
2019-05-04 05:11:33 +0530

    Add Service interfaces

---
+++ src/main/java/com/company/service/H2Service.java
@@ -0,0 +1,5 @@
+package com.company.service;
+
+public interface H2Service {
+    public void getData();
+}
---
+++ src/main/java/com/company/service/RestAPIService.java
@@ -0,0 +1,5 @@
+package com.company.service;
+
+public interface RestAPIService {
+    public void getData();
+}
